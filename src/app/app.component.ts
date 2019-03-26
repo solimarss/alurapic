c4b883f7c8aa5080b09aad0ce8ab53f9eb5f3156
Solimar Silva <solimar.silva@trt8.jus.br>
2019-03-26 15:10:18 -0300

    aula 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'AluraPic';
+  url = 'https://d31j74p4lpxrfp.cloudfront.net/sites/default/files/styles/mobile_large_image/public/br_files/xanda_the_lion_lying.jpg?itok=14IdWu6P';
+  description = 'Leão';
 }
